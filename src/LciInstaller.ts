075a2402fa225cf9d085025b3e3d585568f08384
fabasoad <fabasoad@gmail.com>
2020-11-27 19:46:34 +0900

    Debug 4

--- src/LciInstaller.ts
+++ src/LciInstaller.ts
@@ -1,12 +1,12 @@
 import { addPath } from '@actions/core'
-import { execSync } from 'child_process'
+import { exec, execSync } from 'child_process'
 import fs from 'fs'
 import os from 'os'
 import path from 'path'
 import { Logger } from 'winston'
 import Cache from './Cache'
 import CliExeNameProvider from './CliExeNameProvider'
-import { CMAKE_CLI_NAME, LCI_CLI_NAME } from './consts'
+import { CMAKE_CLI_NAME, LCI_CLI_NAME, MAKE_CLI_NAME } from './consts'
 import ExecutableFileFinder from './ExecutableFileFinder'
 import { clone } from './github'
 import InstallerBase from './InstallerBase'
@@ -18,6 +18,7 @@ export default class LciInstaller extends InstallerBase {
   private _version: string
   private _clone: typeof clone
   private _cmakeProvider: ICliExeNameProvider
+  private _makeProvider: ICliExeNameProvider
   private _lciFinder: IExecutableFileFinder
   private _cache: ICache
   private _log: Logger
@@ -27,12 +28,14 @@ export default class LciInstaller extends InstallerBase {
     co: typeof clone = clone,
     lciProvider: ICliExeNameProvider = new CliExeNameProvider(LCI_CLI_NAME),
     cmakeProvider: ICliExeNameProvider = new CliExeNameProvider(CMAKE_CLI_NAME),
+    makeProvider: ICliExeNameProvider = new CliExeNameProvider(MAKE_CLI_NAME),
     lciFinder: IExecutableFileFinder = new ExecutableFileFinder(LCI_CLI_NAME),
     cache: ICache = new Cache(version, LCI_CLI_NAME)) {
     super(lciProvider)
     this._version = version
     this._clone = co
     this._cmakeProvider = cmakeProvider
+    this._makeProvider = makeProvider
     this._lciFinder = lciFinder
     this._cache = cache
     this._log = LoggerFactory.create('LciInstaller')
@@ -44,23 +47,24 @@ export default class LciInstaller extends InstallerBase {
     const repoDir: string =
       this._clone(owner, repo, `v${this._version}`, this.INSTALL_DIR)
 
-    this._log.info('------- 1 -------')
-    this._log.info(`>> __dirname: ${__dirname}`)
-    this._log.info(`>> process.cwd(): ${process.cwd()}`)
     process.chdir(repoDir)
-    this._log.info('------- 2 -------')
-    this._log.info(`>> __dirname: ${__dirname}`)
-    this._log.info(`>> process.cwd(): ${process.cwd()}`)
-    this.printDir(process.cwd())
 
     this._log.info(`Running > cmake --version`)
-    execSync('cmake --version')
+    exec('cmake --version', (error, stdout, stderr) => {
+      if (error) {
+        console.error(error.message)
+      } else if (stderr) {
+        console.error(stderr)
+      } else {
+        console.log(stdout)
+      }
+    })
 
     const cmd1: string = this._cmakeProvider.getExeFileName() + ' .'
-    this._log.info(`Running > pwd && ${cmd1}`)
+    this._log.info(`Running > ${cmd1}`)
     execSync(cmd1)
 
-    const cmd2: string = 'make'
+    const cmd2: string = this._makeProvider.getExeFileName()
     this._log.info(`Running > ${cmd2}`)
     execSync(cmd2)
 
